abe6b4756eb533d098774c3188945bfadbe72892
Hamit <hamitsehjal26@gmail.com>
2022-10-21 15:01:52 -0400

    fixed a minor bug

--- quick_sort.py
+++ quick_sort.py
@@ -1,20 +1,24 @@
-
 def quick_sort(values):
 
-    # stopping condition for recursion
+    # stopping condition
     if len(values) <= 1:
         return values
 
     # recursive pattern
-    less_than_pivot = []
-    greater_than_pivot = []
     pivot = values[0]
+    less_than_pivot = []
+    more_than_pivot = []
 
     for i in values[1:]:
-        if values[i] <= pivot:
-            less_than_pivot.append(values[i])
+        if i <= pivot:
+            less_than_pivot.append(i)
         else:
-            greater_than_pivot.append(values[i])
+            more_than_pivot.append(i)
+    # print("%15s %1s %-15s" %(less_than_pivot,[pivot],more_than_pivot))
+    return quick_sort(less_than_pivot)+[pivot]+quick_sort(more_than_pivot)
 
-    return quick_sort(less_than_pivot)+[pivot]+quick_sort(greater_than_pivot)
 
+alist = [4, 2, 1, 5, 3, 1, 4]
+
+print("Before Quick-Sort!!: \n", alist)
+print("\nAfter Quick-Sort!!: ", quick_sort(alist))
