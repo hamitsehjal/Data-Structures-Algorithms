612b16013c1cc3f530c2b320ec87830972875949
Hamit <hamitsehjal26@gmail.com>
2022-10-21 08:44:26 -0400

    fixed a minor error. Also added some code to test this paradigm

--- bogo_sort.py
+++ bogo_sort.py
@@ -3,10 +3,12 @@ import random
 
 
 def bogo_sort(list):
+    attempts=0
     while (isSorted(list)==False):
         random.shuffle(list)
+        attempts+=1
     
-    return list
+    return list,attempts
 
 
 def isSorted(list):
@@ -15,7 +17,11 @@ def isSorted(list):
         if(list[i]>list[i+1]):
             return False
         
-        return True
+    return True
 
 
 
+numbers=[5,4,2,12,23,1,9,34]
+print("Before Algorithm: ",numbers)
+
+print("After Algorithm: ",bogo_sort(numbers))
