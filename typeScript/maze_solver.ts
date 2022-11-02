839d6558700c1fa772218fa5ce7768abf5d6575b
Hamit <hamitsehjal26@gmail.com>
2022-11-01 23:30:48 -0400

    added 2nd base case

--- typeScript/maze_solver.ts
+++ typeScript/maze_solver.ts
@@ -8,6 +8,9 @@ function walk(maze:string[],wall:string,curr:Point,end:Point):boolean{
         }
 
     // on a wall
+    if(maze[curr.y][curr.x]===wall){
+        return false;
+    }
 }
 
 export default function solve(maze:string[],wall:string,start:Point, end:Point ):Point[]{
