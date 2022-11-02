3f7673613349f6ea857119c0862815ab065e4284
Hamit <hamitsehjal26@gmail.com>
2022-11-01 23:29:28 -0400

    added First Base Case

--- typeScript/maze_solver.ts
+++ typeScript/maze_solver.ts
@@ -1,5 +1,13 @@
 function walk(maze:string[],wall:string,curr:Point,end:Point):boolean{
-    //
+    // 1. Base Case
+    // off the map
+    if(curr.x<0||curr.x>=maze[0].length||
+        curr.y<0||curr.y>=maze[0].length)
+        {
+            return false;
+        }
+
+    // on a wall
 }
 
 export default function solve(maze:string[],wall:string,start:Point, end:Point ):Point[]{
