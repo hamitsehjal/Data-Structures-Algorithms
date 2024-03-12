ccc33ca570cd85d744c35375c7a7ab164166e3d6
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-03-12 03:53:59 -0400

    add Graph Traversal

--- Algorithms/Divide-and-Conquer/recurseive_sum.py
+++
@@ -1,7 +0,0 @@
-def recursive_sum(arr):
-    # Base case: if an empty array, return 0
-    if len(arr)==0:
-        return 0
-    else:
-        total=arr[0]+sum(arr[1:])
-     return total
--- Algorithms/Divide-and-Conquer/recursive_count.py
+++
@@ -1,9 +0,0 @@
-def count(my_list):
-    if my_list == []:
-        return 0
-    return 1 + count(my_list[1:])
-
-
-my_list = [2, 6, 5]
-
-print(count(my_list))
